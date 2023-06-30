5604d887892bdb6effb9a160f8e7afc40e0f40ae
Justin Lee <justinlee@Justins-Air.cable.rcn.com>
2023-06-30 16:13:33 -0400

    Maze is interactive

--- src/index.ts
+++ src/index.ts
@@ -36,5 +36,51 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   }
 
+  let x = midpoint,
+    y = midpoint;
+
+  function setPosition(newX: number, newY: number) {
+    const xOutOfBounds = newX < 0 || newX >= mazeSize;
+    const yOutOfBounds = newY < 0 || newY >= mazeSize;
+    const notRoom = maze[newX]?.[newY] === undefined;
+
+    if (xOutOfBounds || yOutOfBounds || notRoom) {
+      return;
+    }
+
+    const className = 'has-player';
+
+    function getCell(xPos: number, yPos: number): HTMLElement {
+      return (
+        mazeElement
+          // Offset by 1 as 'nth-of-type' is 1-based while the arrays are 0-based.
+          .querySelector(`.cell:nth-of-type(${yPos * mazeSize + xPos + 1})`)
+      );
+    }
+
+    getCell(x, y).classList.remove(className);
+
+    getCell((x = newX), (y = newY)).classList.add(className);
+  }
+
+  setPosition(x, y);
+
+  document.addEventListener('keydown', (event) => {
+    switch (event.key) {
+      case 'ArrowUp':
+        setPosition(x, y - 1);
+        break;
+      case 'ArrowRight':
+        setPosition(x + 1, y);
+        break;
+      case 'ArrowDown':
+        setPosition(x, y + 1);
+        break;
+      case 'ArrowLeft':
+        setPosition(x - 1, y);
+        break;
+    }
+  });
+
   document.body.appendChild(mazeElement);
 });
